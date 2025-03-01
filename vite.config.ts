4dbc9f7bf658d4710a5b02f335038365eada318a
preciousnwaoha <nwaohaprecious25@gmail.com>
2025-03-01 20:18:59 +0100

    fix: NETLIFY AND VERCEL ERR HANDLING

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig((config) => {
  const env = loadEnv(config.mode, process.cwd());
  return {
    plugins: [react()],
-   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-   },
    server: {
      port: 3000,
      host: true,
